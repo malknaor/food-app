0a208658c7508fbb8f7b1b7ab7b7ece52bad5631
naor.malka <naor.malka@herolo.co.il>
2020-03-15 15:43:42 +0200

    Refactored some code from SearchScreen to useResults hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import yelpApi from '../api/yelp';
+
+const useResults = () => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelpApi.get('/search', {
+                params: {
+                    limit: 50,
+                    location: 'san jose',
+                    term: searchTerm
+                }
+            });
+
+            setSearchResults(response.data.businesses);
+        } catch (error) {
+            setErrorMessage(error.toString());
+        }
+    };
+
+    // Initial Search 
+    useEffect(() => {
+        searchApi('Pizza');
+    });
+
+    return [
+        searchApi,
+        searchResults,
+        errorMessage
+    ];
+};
+
+export default useResults;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,48 +2,29 @@ import React, { useState, useCallback } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
-import yelpApi from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-
-    const searchApi = useCallback(async () => {
-        try {
-            const response = await yelpApi.get('/search', {
-                params: {
-                    limit: 50,
-                    location: 'san jose',
-                    term
-                }
-            });
-
-            setSearchResults(response.data.businesses);
-        } catch (error) {
-            debugger;
-            console.log("searchApi -> error", error);
-        }
-    }, [term, setSearchResults]);
+    const [searchApi, searchResults, errorMessage] = useResults();
 
     return (
         <View>
             <SearchBar
                 searchTerm={term}
                 onSearchTermChange={setTerm}
-                onSearchTermSubmit={searchTerm => searchApi(searchTerm)}
+                onSearchTermSubmit={searchApi}
             />
-            <Text>
-                Search Screen
-            </Text>
-            <Text>
-                {`we have found ${searchResults.length} results`}
-            </Text>
+            {errorMessage? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-
+    errorMessage: {
+        fontSize: 30,
+        color: 'red',
+    }
 });
 
 export default SearchScreen;
